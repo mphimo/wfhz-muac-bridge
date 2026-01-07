9326ef498c55d41da12ed0d2c2bb9ccd3be4308e
tomaszaba <tomas.zaba@outlook.com>
2026-01-07 15:01:03 +0200

    Closes #2; #3; #4; #5

---
+++ scripts/data-wrangling.R
@@ -0,0 +1,87 @@
+# ==============================================================================
+#                              DATA WRANGLING
+# ==============================================================================
+
+
+## ---- Fill all NA's in `survdate` with values from previous cell -------------
+
+surveys <- surveys |> 
+  fill(
+    survdate, 
+    .direction = "updown"
+  ) |> 
+  mutate(
+    survdate = ymd(as.Date(survdate, format = "%d/%m/%Y")),
+    year = year(survdate)
+  )
+  
+
+## ---- Compute z-scores -------------------------------------------------------
+
+### Weight-for-height ----
+data <- surveys |> 
+  mw_wrangle_wfhz(
+    sex = sex,
+    weight = weight,
+    height = height, 
+    .recode_sex = TRUE,
+    .decimals = 3
+  )
+
+### MUAC ----
+data <- data |> 
+  mw_wrangle_age(age = age) |> 
+  mw_wrangle_muac(
+    sex = sex, 
+    .recode_sex = FALSE,
+    muac = muac,
+    .recode_muac = TRUE,
+    .to = "cm",
+    age = age,
+    .decimals = 3
+  )
+
+## ---- Define wasting ---------------------------------------------------------
+
+### Combined case definition ----
+data <- data |> 
+    mutate(
+    muac = recode_muac(muac, "mm")
+  ) |> 
+  define_wasting(
+    zscores = wfhz,
+    muac = muac,
+    oedema = NULL,
+    .by = "combined"
+  )
+
+### Wasting case-definition by WFHZ ----
+  data <- data |> define_wasting(
+    zscores = wfhz,
+    .by = "zscores"
+  ) |> 
+  rename(
+    gam_wfhz = gam,
+    sam_wfhz = sam,
+    mam_wfhz = mam
+  )
+
+### Wasting case-definition by MUAC ----
+data <- data |> 
+  define_wasting(
+    muac = muac,
+    .by = "muac"
+  ) |> 
+  rename(
+    gam_muac = gam, 
+    sam_muac = sam, 
+    mam_muac = mam
+  )
+
+## ---- Filter out outliers by both WFHZ and MFAZ ------------------------------
+
+data <- data |> 
+  filter(flag_wfhz != 1 | flag_mfaz != 1) |> 
+  select(surv_id, year, cluster, cgam, gam_wfhz, gam_muac)
+
+# ============================  End of Workflow ================================
